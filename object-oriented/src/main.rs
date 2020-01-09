87f321313e3ea375a29874e9bcebf8301f79cc50
shuaibc1 <shuaibc1@lenovo.com>
2020-01-09 17:00:14 +0800

    object-oriented: meta traits for comparing

---
+++ object-oriented/src/main.rs
@@ -0,0 +1,45 @@
+use std::cmp::Ordering;
+
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn new() -> Rectangle {
+        Rectangle{
+            width:0,
+            height: 0,
+        }
+    }
+
+    fn area(&self) -> u32 {
+        self.height*self.width
+    }
+}
+
+impl PartialEq for Rectangle {
+    fn eq(&self,rect: &Rectangle) -> bool {
+        return self.area() == rect.area();
+    }
+}
+impl PartialOrd for Rectangle {
+    fn partial_cmp(&self, rect: &Rectangle) -> Option<Ordering> {
+        let a1 = self.area();
+        let a2 = rect.area();
+        return Some(a1.cmp(&a2))
+    }
+}
+
+fn main() {
+    let mut rect = Rectangle::new();
+    rect.width = 10;
+    rect.height = 10;
+
+    let mut rect2 = Rectangle::new();
+    rect2.width = 9;
+    rect2.height = 8;
+
+    assert_eq!(rect>rect2, true);
+    println!("the area is {}", rect.area());
+}
--- variable/src/main.rs
+++ variable/src/main.rs
@@ -9,11 +9,18 @@ fn test_move() {
     let s1 = String::from("hello");
     let s2 = s1;
     //println!("{}, world!", s1);
-    let s3 = s2.clone();
+    let mut s3 = s2.clone();
     println!("{},{}, world!", s2,s3);
-
-    test_fn_argument(s3);
+    //test_fn_argument(s3);
     //println!("{},world!", s3);
+
+    let s4 = &mut s3;
+    let c1 = s4.len();
+    println!("c1 is {}", c1);
+
+    let s5 = &mut s3;
+    let c2 = s5.len();
+    println!("c2 is {}", c2);
 
     test_borrow_change(&mut s3)
 }
